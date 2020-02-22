02387aa0ecbf13140d96fab3596af0dd7639c47f
henry <toyokoyo199@gmail.com>
2020-02-22 14:16:54 +0800

    Initial Commit

---
+++ TripApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripApp
+//
+//  Created by 廖冠翰 on 2020/2/22.
+//  Copyright © 2020 廖冠翰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
